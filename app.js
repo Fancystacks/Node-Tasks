f46e8e54d65e80c268694557c2432b5b15542aef
Leah Migacz <lmigacz@msudenver.edu>
2020-04-08 17:52:25 -0600

    added requires

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+const path = require('path');
+const db = require('./db');
+const collection = "todo";
