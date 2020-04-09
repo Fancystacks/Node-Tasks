a4c18b10ec0b8f6c7b6a0fe2986856055c77f0de
Leah Migacz <lmigacz@msudenver.edu>
2020-04-08 18:32:33 -0600

    added get route 😃

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const db = require('./db');
 const collection = "todo";
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getTodos', (req, res) = {
+    db.getDB().collection(collection).find({}).toArray((err, documents) => {
+        if (err)
+        console.log(err);
+    })
+});
+
 db.connect((err) => {
     if (err) {
     console.log("Can't connect to database");
