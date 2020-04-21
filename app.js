2968f98ad88ee18be72d6e9722d580f494ef0530
Leah Migacz <lmigacz@msudenver.edu>
2020-04-20 19:05:31 -0600

    fixed things

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(bodyParser.json());
 const path = require('path');
 const db = require('./db');
 const collection = "todo";
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
